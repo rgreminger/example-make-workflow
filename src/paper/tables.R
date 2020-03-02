a9427cb7df3dd4d82c9a2f18a68a0e4e6b4fa09b
R. Greminger <rafael.greminger@outlook.com>
2020-03-02 08:15:38 +0100

    fixed table

--- src/paper/tables.R
+++ src/paper/tables.R
@@ -4,5 +4,5 @@ load("./gen/analysis/output/model_results.RData")
 # Load in additional package to export to latex table
 require(stargazer) 
 
-# Export to latex table 
-stargazer(m1,m2,out="./gen/paper/output/table1.tex")
+# Export to latex table (omits f-stat since messes up table)
+stargazer(m1,m2,out="./gen/paper/output/table1.tex",omit.stat=c("f")) 
