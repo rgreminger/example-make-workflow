ae252d3746f7116a826c2403e82b567d1ca3bae2
rgreminger <rafael.greminger@outlook.com>
2020-04-17 15:46:19 +0200

    delete files in temp directories
    add Rhistory to delete
    
    bugfix

--- src/clean-up.R
+++ src/clean-up.R
@@ -1,9 +1,15 @@
 # Deletes files in all subdirectories with the endings specified here
-fileEndings <- c('*.log','*.aux','*.Rout')
+fileEndings <- c('*.log','*.aux','*.Rout','*.Rhistory')
 for (fi in fileEndings) { 
-  files <- list.files(getwd(),fi,include.dirs=F,recursive=T,full.names=T)
+  files <- list.files(getwd(),fi,include.dirs=F,recursive=T,full.names=T,all.files=T)
   file.remove(files)
 }
+
+# Delete all files in temp directories
+# (does note delete hidden files starting with . (e.g. .gitkeep is not deleted))
+unlink(paste(getwd(),'/gen/analysis/temp/*',sep=''),recursive=T,force=T)
+unlink(paste(getwd(),'/gen/data-preparation/temp/*',sep=''),recursive=T,force=T)
+unlink(paste(getwd(),'/gen/paper/temp/*',sep=''),recursive=T,force=T)
 
 # Delete temporary (hidden) R files
 file.remove('.RData')
